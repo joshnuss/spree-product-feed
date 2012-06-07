11138f5548a5ad443ee8633239491509eacc39d8
Jason Burfield <jason@burfield.com>
2012-06-07 12:04:26 -0400

    removed caching...seems like it would be an issue in multi-server environments

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -1,5 +1,5 @@
 Spree::ProductsController.class_eval do
   respond_to :rss, :only => [:index]
 
-  caches_page :index, :if => Proc.new {|c| c.request.format.rss? }
+  # caches_page :index, :if => Proc.new {|c| c.request.format.rss? }
 end
